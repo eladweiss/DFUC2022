d96a5fa5f8395726af3c4cec6fc1ad9365078d77
Elad <eladwei@gmail.com>
2026-03-23 10:07:51 +0200

    main: Fix file path concatanation to use os.path.join().
    Signed-off-by: Elad <eladwei@gmail.com>

--- evaluate.py
+++ evaluate.py
@@ -6,8 +6,8 @@ import argparse
 
 class test_dataset:
     def __init__(self, image_root, gt_root):
-        self.images = [image_root + f for f in os.listdir(image_root) if f.endswith('.jpg') or f.endswith('.png')]
-        self.gts = [gt_root + f for f in os.listdir(gt_root) if f.endswith('.jpg') or f.endswith('.png')]
+        self.images = [os.path.join(image_root, f) for f in os.listdir(image_root) if f.endswith('.jpg') or f.endswith('.png')]
+        self.gts = [os.path.join(gt_root, f) for f in os.listdir(gt_root) if f.endswith('.jpg') or f.endswith('.png')]
         self.images = sorted(self.images)
         self.gts = sorted(self.gts)
         self.transform = transforms.ToTensor()
